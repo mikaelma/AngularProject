21ad2a2ea8e763ecd9d1962ce923626b6f4e562c
Mikael Mathisen <mikaelmathisen@dhcp-10-22-45-162.wlan.ntnu.no>
2017-11-09 14:51:31 +0100

    Header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,28 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {routing} from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { MyPageComponent } from './my-page/my-page.component';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule, MatDialogModule, MatButtonModule, MatIconModule} from '@angular/material';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MyPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    routing
+    routing,
+    MatToolbarModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
