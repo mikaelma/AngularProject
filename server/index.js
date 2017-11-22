25532e7019e60e50026b865a7d09b484df3a8c2b
root <matzwiik@gmail.com>
2017-11-22 16:48:55 +0100

    changed port

--- server/index.js
+++ server/index.js
@@ -374,7 +374,7 @@ app.get('*', function (req, res) {
 });
 
 
-var server = app.listen(8080, () => {
+var server = app.listen(8084, () => {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Running on 8080');
